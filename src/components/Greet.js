a963578dcc7c4ab63471a1f894903cd8e5b2461c
sserges <agounkeserge@gmail.com>
2021-10-21 13:01:13 +0000

    Functional Components

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+// function Greet() {
+//     return <h1>Hello Vishwas !</h1>
+// }
+
+// const Greet = () => {
+//     return <h1>Hello Vishwasssss !</h1>
+// }
+
+export const Greet = () => <h1>Hello Vishwaxy!</h1>
+
+// export default Greet
