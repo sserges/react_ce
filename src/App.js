f78cbe468e3a8a02334d6b8ebabfa6c93d1a8a12
sserges <agounkeserge@gmail.com>
2021-12-13 21:13:15 -0400

    JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
 
-import { Greet } from './components/Greet'
-import Welcome from './components/Welcome'
+// import { Greet } from './components/Greet'
+import Hello from './components/Hello'
+// import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className='App'>
-      <Greet />
-      <Welcome />
+      {/* <Greet />
+      <Welcome /> */}
+
+      <Hello />
     </div>
   )
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hello = () => {
+  //   return (
+  //     <div>
+  //       <h1>Hello Richard</h1>
+  //     </div>
+  //   )
+  return React.createElement(
+    'div',
+    { id: 'hello', className: 'dummyClass' },
+    React.createElement('h1', null, 'Hello Vishwas')
+  )
+}
+
+export default Hello
