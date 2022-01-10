e47312c15b6036b78a000257046c73b67d3c7c47
sserges <agounkeserge@gmail.com>
2022-01-10 17:12:26 -0500

    setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css'
+import Counter from './components/Counter'
 
-import { Greet } from './components/Greet'
-import Message from './components/Message'
+// import { Greet } from './components/Greet'
+// import Message from './components/Message'
 // import Hello from './components/Hello'
-import Welcome from './components/Welcome'
+// import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className='App'>
-      <Message />
+      <Counter />
+      {/* <Message />
       <Greet name='Bruce' heroName='Batman'>
         <p>This is children props</p>
       </Greet>
@@ -18,7 +20,7 @@ function App() {
       <Greet name='Diana' heroName='Wonder Woman' />
       <Welcome name='Bruce' heroName='Batman' />
       <Welcome name='Clark' heroName='Superman' />
-      <Welcome name='Clark' heroName='Superman' />
+      <Welcome name='Clark' heroName='Superman' /> */}
 
       {/* <Hello /> */}
     </div>
