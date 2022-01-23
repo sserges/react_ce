d3dcbcc111235446eb73ce7418d514da468785fc
sserges <agounkeserge@gmail.com>
2022-01-23 11:53:40 -0500

    Binding Event handler

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,9 +7,18 @@ class EventBind extends Component {
     this.state = {
       message: 'Hello',
     }
+
+    // this.clickHandler = this.clickHandler.bind(this)
   }
 
-  clickHandler() {
+  //   clickHandler() {
+  //     this.setState({
+  //       message: 'GoodBye',
+  //     })
+  //     console.log(this)
+  //   }
+
+  clickHandler = () => {
     this.setState({
       message: 'GoodBye',
     })
@@ -21,7 +30,8 @@ class EventBind extends Component {
       <div>
         <div>{this.state.message}</div>
         {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
-        <button onClick={() => this.clickHandler()}>Click</button>
+        {/* <button onClick={() => this.clickHandler()}>Click</button> */}
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     )
   }
