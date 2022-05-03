a22df8818dace85ab07c9f637d0018698d0a150a
sserges <agounkeserge@gmail.com>
2022-05-03 18:56:51 -0400

    Fragments

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import './App.css'
-// import Inline from './components/Inline'
-// import NameList from './components/NameList'
-// import Stylesheet from './components/Stylesheet'
-// import ClassClick from './components/ClassClick'
-// import FunctionClick from './components/FunctionClick'
-// import EventBind from './components/EventBind'
-// import ParentComponent from './components/ParentComponent'
-// import UserGreeting from './components/UserGreeting'
-// import Counter from './components/Counter'
-
-// import { Greet } from './components/Greet'
-// import Message from './components/Message'
-// import Hello from './components/Hello'
-// import Welcome from './components/Welcome'
 
 import './appStyles.css'
-// import styles from './appStyles.module.css'
-// import Form from './components/Form'
-import LifecycleA from './components/LifecycleA'
+// import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
 
 function App() {
   return (
     <div className='App'>
-      <LifecycleA />
+      {/* <FragmentDemo /> */}
+      <Table />
     </div>
   )
 }
---
+++ src/components/FragmentDemo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FragmentDemo() {
+  return (
+    <>
+      <h1>FragmentDemo</h1>
+      <p>This describes the fragment demo Component</p>
+    </>
+  )
+}
+
+export default FragmentDemo
